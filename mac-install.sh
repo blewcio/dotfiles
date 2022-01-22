c1c5c494fceec59deeae3fa5981f1aa260c69fc9
Blazej Lewcio <p362513@blewcioersatz.fritz.box>
2022-01-22 11:44:24 +0100

    Improved brew lists

--- mac-install.sh
+++ mac-install.sh
@@ -1,53 +1,90 @@
 # Install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+if [ ! $? -eq 0 ]; then
+  echo "Homebrew not installed. Aborting..."
+  return -1
+fi
+
 # Install productivity helpers
 brew install --cask \
  rectangle \     # Move windows around with keys
  alt-tab   \     # Windows like tab
  alfred    \     # Additional search and much more
- pock            # Touchbar config
+ pock      \     # Touchbar config
+ cheatsheet      # Show shotcuts on screen
+# customssshortcut # Define own shortcuts in Apps
+
+if [ $? -eq 0 ]; then
+  echo "Productivity helpers installed successfully"
+fi
 
 # Install basic tools
 brew install --cask \
   iterm2 \
-  mysides \ # Simple CLI based sidebar configuration
-  chrome-browser \
-  brave-browser  \
+  macvim \
+  google-chrome \
+  amazon-photos  \
+  messenger \
   miro \
-  gimp \
-  gnumeric
-
-# Install additional tools
-brew install --cask \
-  juststream \
-  # NX Studio
-  # Brother P touch editor
+  zoom \
+  gimp 
 
 # Install basic tools for improved terminal
 brew install \
-  bat \  # Improved cat
-  exa \  # Improved ls
-  tree\
-  jq\
-  htop\
-  fzf\
-  fasd\
-  jdupes\
-  ripgrep\
-  tmux\
-  wakeonlan\
-  nmap \ # Network scanner
-  ranger\
-  ffmpeg\
-  mas \  #install from Appstore via Terrminal)
-  speedtesst-cli\
-  terminal-notifier
+  git        \
+  bat        \  # Improved cat
+  exa        \  # Improved ls
+  tree       \
+  tldr       \  # Improved man
+  jq         \    # Commandline JSON processor
+  htop       \  # Improved top
+  jdupes     \ # Duplicate file finder
+  fzf        \   # Fuzzy finder
+  fasd       \  # Quick access to (recent) files and directories
+  ripgrep    \ # Improved grep
+  tmux       \ # Improvee screen
+  wakeonlan  \
+  nmap       \ # Network scanner
+  ranger     \ # Vi like file browser
+  mas        \  #install from Appstore via Terrminal)
+  speedtesst-cli \ Network speed test
+  terminal-notifier \ # Notify via notification center
+  ffmpeg
 
 # TODO: Config fzf & fasd
 # TODO: Config zsh
 # Plugins
 # Powerline
 
+# Install quick look plugins
+brew install --cask \
+    qlcolorcode \
+    qlstephen \
+    qlmarkdown \
+    qlvideo \
+    quicklook-json \
+    qlprettypatch \
+    quicklook-csv \
+    suspicious-package \
+    betterzip \
+    webpquicklook
 
-pock
+mas install 1437630108  # HomeAtmo Lite
+mas install 416581096   # QuickCal for widget in notification center
+mas install 1453365242  # Brothere P Touch
+mas install 1534707384  # Hue App or we love lights TODO
+mas install 1474804779  # Juststream for file to Chromecast streaming
+
+# Install additional tools
+brew install --cask \
+  appcleaner \
+  mysides \ # Simple CLI based sidebar configuration
+  brave-browser  \
+  caffeine \ # Dont let mac sleep
+  android-file-transfer \
+  anydo 
+
+brew install \
+  gnumeric
+
