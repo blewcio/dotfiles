5a062a57aee5849d4f8acd79145e5467dc6401af
Blazej Lewcio <blazej.lewcio@gmail.com>
2025-01-25 17:52:36 +0100

    Local bin/ must be added before aliases are loaded.

--- shellrc.sh
+++ shellrc.sh
@@ -4,6 +4,9 @@ case $- in
       *) return;;
 esac
 
+# Add sbin and bin in $HOME `$PATH`
+export PATH="$HOME/bin:$HOME/sbin:$PATH"
+
 # Source all config files from shell config folder
 SHELL_CONFIG_DIR=$HOME/dotfiles/shell.d
 if [ -x $SHELL_CONFIG_DIR ]; then
