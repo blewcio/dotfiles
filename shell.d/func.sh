7ddf9cfb1490df1c4fa5f4a257ea36066afca3a6
Blazej Lewcio <blazej.lewcio@gmail.com>
2025-02-11 18:57:52 +0100

    Starting conditions in func.sh and more aliasing (for easy listing of commands)

--- shell.d/func.sh
+++ shell.d/func.sh
@@ -1,18 +1,28 @@
 ## CSV Editing
 # Display CSV file with proper columns (tabulated)
-csvcat() {
-  cat $1 | sed -e 's/,,/, ,/g' | column -s, -t | less -#5 -N -S 
+csv_cat() {
+  if [ -z $1 ] || [ ! -f $1 ]; then
+    echo "Incorrect filename. Usage: function FILENAME"
+    return -1
+  fi
+  cat $1 | sed -e 's/,,/, ,/g' | column -s, -t # | less -#5 -N -S 
 }
+alias cat-csv=csv_cat
 
 # Display CSV with csvlook (as a table)
-csvcat2() {
+csv_cat2() {
   if ! [ -x "$(command -v csvlook)" ]; then "csvkit not installed"; return -1; fi
+  if [ -z $1 ] || [ ! -f $1 ]; then
+    echo "Incorrect filename. Usage: function FILENAME"
+    return -1
+  fi
   csvlook --max-column-width=21 $1 | bat -p -l rs
 }
+alias cat-csv2=csv_cat2
 
 ## System functions or commands
 # Safe remove
-copy-to-trash() {
+copy_to_trash() {
   local TRASH
   TRASH=~/.Trash/
   len=$#
@@ -34,93 +44,100 @@ copy-to-trash() {
 mkcd() {
   mkdir -p "$@" && cd "$_"
 }
+alias mkcd=mkcd
 
 # Interactive list of most recent directories (param to prefilter)
 # then cd
-t() {
-  local fasdlist
-  fasdlist=$(fasd -d -l -r $1 | \
-    fzf --query="$1 " --select-1 --exit-0 --height=25% --reverse --tac --no-sort --cycle) &&
-    cd "$fasdlist"
-}
+if [[ -x "$(command -v fzf)" ]] && [[ -x "$(command -v fasd)" ]]; then
+  find_recent_dir() {
+    local fasdlist
+    fasdlist=$(fasd -d -l -r $1 | \
+      fzf --query="$1 " --select-1 --exit-0 --height=25% --reverse --tac --no-sort --cycle) &&
+      cd "$fasdlist"
+  }
+  alias t=find_recent_dir
+fi
 
 # Use ripgrep and fzf to search file content
 # Find in files
-fif() {
+if [[ -x "$(command -v fzf)" ]] && [[ -x "$(command -v fasd)" ]]; then
+  find_in_files() {
    rg --column --line-number --no-heading --color=always --smart-case "${*:-}" |
    fzf --ansi -d ':' --preview 'bat --style=numbers --color=always --highlight-line {2} {1}' --preview-window='50%' --preview-window '~3:+{2}+3/2' \
        --header=$'Press ? to preview\n      Enter/CTRL-E to edit\n      CTRL-Y to yank filename\n\n' --header-lines=0 \
        --bind 'ctrl-y:execute-silent(echo {1} | pbcopy)' --bind 'enter:become(vim {1} +{2})' --bind 'ctrl-e:execute(vim {1} +{2})' 
   # [ -n "$selected" ] && $EDITOR "$selected" # not needed
-}
+  }
+  alias fif=find_in_files
+fi
 
 # `tree2` is a shorthand for `tree` with hidden files and color enabled, ignoring
 # the `.git` directory, listing directories first. The output gets piped into
 # `less` with options to preserve color and line numbers, unless the output is
 # small enough for one screen.
-tree2() {
-  tree -aC -I '.git|node_modules|bower_components' --dirsfirst "$@" | less -FRNX;
-}
+if [[ -x "$(command -v tree)" ]]; then
+ tree2() {
+   tree -aC -I '.git|node_modules|bower_components' --dirsfirst "$@" | less -FRNX;
+ }
+ alias tree2=tree2
+fi
 
 # Change working directory to the top-most Finder window location
-cd-finder() { 
-  cd "$(osascript -e 'tell app "Finder" to POSIX path of (insertion location as alias)')";
-}
+if [[ "$(uname)" == "Darwin"  ]]; then
+  open_finder_directory() { 
+    cd "$(osascript -e 'tell app "Finder" to POSIX path of (insertion location as alias)')";
+  }
+  alias cd-finder=open_finder_directory
+fi
 
 # Time now
-now() {
+time_now() {
   echo -n "Now is "
   date +"%X, %A, %B %-d, %Y"
 }
+alias now=time_now
 
 # Status of the computer
-status() {
+net_status() {
   echo -e "\nUptime:"
-  uptime -p | sed 's/^/\t/'
+  uptime | sed 's/^/\t/'
   echo -e "Active users:"
   users | sed 's/^/\t/'
   echo -e "Open ports:"
   lsof -Pni4 | grep LISTEN | sed 's/^/\t/'
-  # echo -e "Memory:"
-  # free -hm | sed 's/^/\t/'
-  # echo -e "Disk space:"
-  # df -h 2> /dev/null | sed 's/^/\t/'
-  # echo -e "Disk inodes:"
-  # df -i 2> /dev/null | sed 's/^/\t/'
-  # echo -e "Block devices:"
-  # lsblk | sed 's/^/\t/'
-  # if [[ -r /var/log/syslog ]]; then
-  # echo -e "Syslog:"
-  # tail /var/log/syslog | sed 's/^/\t/'
-  # fi
-  # if [[ -r /var/log/messages ]]; then
-  # echo -e "Messages:"
-  # tail /var/log/messages | sed 's/^/\t/'
-  # fi
 }
+alias net-status=net_status
 
 # Remove all installed brew packages
-brew-uninstall-all() {
-  echo "Purging all installed brew packages"
-  read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
-  while [[ `brew list | wc -l` -ne 0 ]]; do
-    for EACH in `brew list`; do
-      brew uninstall --force --ignore-dependencies $EACH
+if [[ "$(uname)" == "Darwin"  ]]; then
+  brew_uninstall_all() {
+    echo "Purging all installed brew packages"
+    printf "Continue? (Y/N): "
+    read confirm 
+
+    # Stop here if not confirmed
+    [[ ! $confirm == [yY] ]] && return 1
+
+    while [[ `brew list | wc -l` -ne 0 ]]; do
+      for EACH in `brew list`; do
+        brew uninstall --force --ignore-dependencies $EACH
+      done
     done
-  done
-  echo << EOT
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
+    echo << EOT
+    # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
 EOT
-}
+  }
+  alias brew-uninstall-all=brew_uninstall_all
+fi
 
 # nnn wrapper to stay in directory after exit
 if [ -x "$(command -v nnn)" ]; then
-n () {
+  n () {
     # Block nesting of nnn in subshells
     [ "${NNNLVL:-0}" -eq 0 ] || {
-        echo "nnn is already running"
-        return
-    }
+      echo "nnn is already running"
+          return
+        }
 
     # The behaviour is set to cd on quit (nnn checks if NNN_TMPFILE is set)
     # If NNN_TMPFILE is set to a custom path, it must be exported for nnn to
@@ -143,17 +160,17 @@ n () {
         . "$NNN_TMPFILE"
         rm -f -- "$NNN_TMPFILE" > /dev/null
     }
-}
-
+  }
+  alias n=n
 fi
 
 if [ -x "$(command -v yazi)" ]; then
-function y() {
-  local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
-  yazi "$@" --cwd-file="$tmp"
-  if cwd="$(command cat -- "$tmp")" && [ -dedicatedn "$cwd"  ] && [ "$cwd" != "$PWD"  ]; then
-    builtin cd -- "$cwd"
-  fi
-  rm -f -- "$tmp"
-}
+  y() {
+    local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+    yazi "$@" --cwd-file="$tmp"
+    if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+      cd "$cwd"
+    fi
+    rm $tmp }
+  alias y=y
 fi
