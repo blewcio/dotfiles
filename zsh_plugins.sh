2faeb0634691f417cbb0dd473aef981b6cf8b77c
Blazej Lewcio <blazej.lewcio@gmail.com>
2026-01-08 12:46:54 +0100

    Versioning of oh-my-zsh plugin list.

---
+++ zsh_plugins.sh
@@ -0,0 +1,4 @@
+
+# This is a list of plugins loaded by oh-my-zsh and loaded in .zshrc before
+# startgin oh-my-zsh
+plugins=(fasd alias-finder virtualenv direnv git zsh-autosuggestions zsh-syntax-highlighting fast-syntax-highlighting)
